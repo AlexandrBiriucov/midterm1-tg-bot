78d23d34a5e9b661c39260baab0ddb7da5d5af34
AlexandrBiriucov <saneok.biriucov@gmail.com>
2025-11-28 05:40:07 -0700

    refactor

--- main/feature/dev2_exercise_library/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Dev2 Exercise Library Module
-Provides exercise database and filtering functionality
-"""
-
-from .exercise_db import ExerciseDatabase
-from .exercise_handlers import exercise_router
-
-__all__ = ['ExerciseDatabase', 'exercise_router']
